2f022622441f79c1c8067db9ce942097306fec73
Tr3-Tr8n <tri030411@gmail.com>
2025-08-10 04:04:42 +0000

    1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-image = 'https://vi.wikipedia.org/wiki/%C4%90en_(rapper)'
+image = 'https://th.bing.com/th/id/R.06eb44e9c22d2084864e7341295f5662?rik=O%2brhFDM0ViqqJw&riu=http%3a%2f%2fvietcetera.com%2fwp-content%2fuploads%2f2017%2f06%2fDen-Vau.jpg&ehk=VvjwBRxfo5WzpBWDPZliVKhoCBj5tLXCEonGn%2bntZk8%3d&risl=&pid=ImgRaw&r=0'
 st.image(image, caption = 'Den vau')
     
 
